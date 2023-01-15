f2ff929749babdabe4c73e606967708b41832ed8
still-lava115 <36177833+still-lava115@users.noreply.github.com>
2023-01-14 20:02:10 -0500

    Cleaned up code, bugfixes, new end message

--- 2048.py
+++ 2048.py
@@ -1,126 +1,160 @@
 import random
 
-alive = True
-board = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+SIZE = 2
+PROB_FOUR = 0.5
 
-for i in range(100):
-    print()
+def get_empty_spaces(board):
+	empty_spaces = []
+	for i in range(SIZE):
+		for j in range(SIZE):
+			if board[i][j] == 0:
+				empty_spaces.append((i,j))
+	return empty_spaces
 
-for i in range(2):
-    emptySpaces = []
-    for i in range(4):
-        for j in range(4):
-            emptySpaces.append((i,j))
-    
-    space = random.randint(0,len(emptySpaces)-1)
-    
-    if random.random() > 0.9:
-        board[emptySpaces[space][0]][emptySpaces[space][1]] = 4
-    else:
-        board[emptySpaces[space][0]][emptySpaces[space][1]] = 2
+# modifies board itself
+def add_tile(board, empty_spaces):
+	space = empty_spaces[random.randint(0,len(empty_spaces)-1)]
+	
+	if random.random() < PROB_FOUR:
+		board[space[0]][space[1]] = 4
+	else:
+		board[space[0]][space[1]] = 2
 
-while (alive):
-    for i in board:
-        for j in i:
-            print(j, end = '')
-            if j < 10:
-                print('    ', end = '')
-            elif j < 100:
-                print('   ', end = '')
-            elif j < 1000:
-                print('  ', end = '')
-            else:
-                print(' ', end = '')
-        print()
-    
-    uin = input("")
-    
-    move = False
-    if uin == "w":
-        for i in range(4):
-            for j in range(4):
-                k = j
-                if board[j][i] != 0:
-                    while k > 0:
-                        if board[k-1][i] == 0:
-                            board[k-1][i] = board[k][i]
-                            board[k][i] = 0
-                            k-=1
-                            move = True
-                        else:
-                            if board[k-1][i] == board[k][i]:
-                                board[k-1][i] = 2*board[k][i]
-                                board[k][i] = 0
-                                move = True
-                            break
-    elif uin == "s":
-        for i in range(4):
-            for j in range(4):
-                k = j
-                if board[3-k][i] != 0:
-                    while k > 0:
-                        if board[3-k+1][i] == 0:
-                            board[3-k+1][i] = board[3-k][i]
-                            board[3-k][i] = 0
-                            k-=1
-                            move = True
-                        else:
-                            if board[3-k+1][i] == board[3-k][i]:
-                                board[3-k+1][i] = 2*board[3-k][i]
-                                board[3-k][i] = 0
-                                move = True
-                            break
-    elif uin == "a":
-        for i in range(4):
-            for j in range(4):
-                k = j
-                if board[i][k] != 0:
-                    while k > 0:
-                        if board[i][k-1] == 0:
-                            board[i][k-1] = board[i][k]
-                            board[i][k] = 0
-                            k-=1
-                            move = True
-                        else:
-                            if board[i][k-1] == board[i][k]:
-                                board[i][k-1] = 2*board[i][k]
-                                board[i][k] = 0
-                                move = True
-                            break
-    elif uin == "d":
-        for i in range(4):
-            for j in range(4):
-                k = j
-                if board[i][3-k] != 0:
-                    while k > 0:
-                        if board[i][3-k+1] == 0:
-                            board[i][3-k+1] = board[i][3-k]
-                            board[i][3-k] = 0
-                            k-=1
-                            move = True
-                        else:
-                            if board[i][3-k+1] == board[i][3-k]:
-                                board[i][3-k+1] = 2*board[i][3-k]
-                                board[i][3-k] = 0
-                                move = True
-                            break
-    else:
-        move = True
-    
-    emptySpaces = []
-    if move:
-        for i in range(4):
-            for j in range(4):
-                if board[i][j] == 0:
-                    emptySpaces.append((i,j))
-                    
-    if len(emptySpaces) > 0:
-        space = random.randint(0,len(emptySpaces)-1)
+def initialize_board(SIZE):
+	board = [[0]*SIZE for i in range(SIZE)]
+	for i in range(2):
+		empty_spaces = get_empty_spaces(board)
+		add_tile(board, empty_spaces)
+	
+	return board
 
-        if random.random() > 0.9:
-            board[emptySpaces[space][0]][emptySpaces[space][1]] = 4
-        else:
-            board[emptySpaces[space][0]][emptySpaces[space][1]] = 2
-    else:
-        alive = False
-    
+def print_board(board):
+	for i in board:
+		for j in i:
+			if j == 0:
+				print('-'+' '*4, end = '')
+			elif j < 10:
+				print(str(j)+' '*4, end = '')
+			elif j < 100:
+				print(str(j)+' '*3, end = '')
+			elif j < 1000:
+				print(str(j)+' '*2, end = '')
+			else:
+				print(str(j)+' ', end = '')
+		print()
+
+def move(board, input):
+	successful = False
+
+	if input == "w":
+		for i in range(SIZE):
+			for j in range(SIZE):
+				k = j
+				if board[j][i] != 0:
+					while k > 0:
+						if board[k-1][i] == 0:
+							board[k-1][i] = board[k][i]
+							board[k][i] = 0
+							k-=1
+							successful = True
+						else:
+							if board[k-1][i] == board[k][i]:
+								board[k-1][i] = 2*board[k][i]
+								board[k][i] = 0
+								successful = True
+							break
+	elif input == "s":
+		for i in range(SIZE):
+			for j in range(SIZE):
+				k = j
+				if board[SIZE-1-k][i] != 0:
+					while k > 0:
+						if board[SIZE-k][i] == 0:
+							board[SIZE-k][i] = board[SIZE-1-k][i]
+							board[SIZE-1-k][i] = 0
+							k-=1
+							successful = True
+						else:
+							if board[SIZE-k][i] == board[SIZE-1-k][i]:
+								board[SIZE-k][i] = 2*board[SIZE-1-k][i]
+								board[SIZE-1-k][i] = 0
+								successful = True
+							break
+	elif input == "a":
+		for i in range(SIZE):
+			for j in range(SIZE):
+				k = j
+				if board[i][k] != 0:
+					while k > 0:
+						if board[i][k-1] == 0:
+							board[i][k-1] = board[i][k]
+							board[i][k] = 0
+							k-=1
+							successful = True
+						else:
+							if board[i][k-1] == board[i][k]:
+								board[i][k-1] = 2*board[i][k]
+								board[i][k] = 0
+								successful = True
+							break
+	elif input == "d":
+		for i in range(SIZE):
+			for j in range(SIZE):
+				k = j
+				if board[i][SIZE-1-k] != 0:
+					while k > 0:
+						if board[i][SIZE-k] == 0:
+							board[i][SIZE-k] = board[i][SIZE-1-k]
+							board[i][SIZE-1-k] = 0
+							k-=1
+							successful = True
+						else:
+							if board[i][SIZE-k] == board[i][SIZE-1-k]:
+								board[i][SIZE-k] = 2*board[i][SIZE-1-k]
+								board[i][SIZE-1-k] = 0
+								successful = True
+							break
+	
+	return successful
+
+def is_board_alive(board):
+	empty_spaces = get_empty_spaces(board)
+	if len(empty_spaces) > 0: return True
+	for i in range(SIZE):
+		for j in range(SIZE-1):
+			if board[i][j] == board[i][j+1]: return True
+	for i in range(SIZE-1):
+		for j in range(SIZE):
+			if board[i][j] == board[i+1][j]: return True
+	return False
+
+def main():
+	alive = True
+
+	board = initialize_board(SIZE)
+
+	# clear screen
+	for i in range(100):
+		print()
+
+	while is_board_alive(board):
+		
+		print_board(board)
+		
+		uin = input("")
+		print("")
+		
+		successful = move(board,uin)
+		
+		if successful:
+			empty_spaces = get_empty_spaces(board)
+			# guaranteed to be at least one space
+			add_tile(board, empty_spaces)
+			
+	# after death
+	
+	print_board(board)
+	print('Game over!')
+	
+			
+main()
---
+++ README.md
@@ -0,0 +1,4 @@
+# python-2048
+
+A python implementation of the classic game, 2048.
+Change the board size by modifying `SIZE` and the probability of generating a 4 tile instead of a 2 tile with by modifying `PROB_FOUR`.
